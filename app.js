1ef2945a30dce6824ccbf3f40a9a053e807ea755
PabloolbaP-23 <pgarcsim2334@hotmail.com>
2023-05-24 08:09:31 +0200

    Cambios acualizados (array)

--- app.js
+++ app.js
@@ -3,32 +3,37 @@ const template = document.getElementById("template");
 const fragment = document.createDocumentFragment();
 const btnesBotones = document.querySelectorAll('.card .btn');
 
-const carritoObjeto = { // OBJETO
+const carritoObjeto = [];
 
-}
 const agregarAlCarrito = (e) => { 
-    console.log(e.target.dataset.fruta);
     const producto = {
         titulo: e.target.dataset.fruta,
         id: e.target.dataset.fruta,
         cantidad: 1
     }
-    if (carritoObjeto.hasOwnProperty(producto.titulo)) {
-        producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1
-    }
-    carritoObjeto[producto.titulo] = producto
-    pintarCarrito()
-}
-btnesBotones.forEach((btn) => btn.addEventListener("click",agregarAlCarrito));
 
-const pintarCarrito = () => {
-    //console.log("pintar carrito", producto)
-    carrito.textContent = ""
-    Object.values(carritoObjeto).forEach(item => {
+    const indice = carritoObjeto.findIndex(
+        (item) => item.id === producto.id
+    );
+    console.log(indice);
+    if (indice === -1) {
+        carritoObjeto.push(producto);
+    } else {
+        carritoObjeto[indice].cantidad ++
+    }
+    console.log(carritoObjeto)
+    pintarCarrito(carritoObjeto);
+}
+
+btnesBotones.forEach((btn) => btn.addEventListener("click", agregarAlCarrito));
+
+const pintarCarrito = (array) => {
+    carrito.textContent = "";
+    array.forEach(item => {
         const clone = template.content.firstElementChild.cloneNode(true);
-        clone.querySelector('.lead').textContent = item.titulo
-        clone.querySelector('.badge').textContent = item.cantidad
-        fragment.appendChild(clone)
-    })
-    carrito.appendChild(fragment)
+        clone.querySelector('.lead').textContent = item.titulo;
+        clone.querySelector('.badge').textContent = item.cantidad;
+        fragment.appendChild(clone);
+    });
+    carrito.appendChild(fragment);
 }
